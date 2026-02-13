81ba6496bf005243e66911a2b74fcc23bd727e4f
Nishant Chauhan <nnishant0308chauhan@gmail.com>
2026-02-13 23:15:29 +0530

    huhu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import Library from "./Experiments/exp3_2/Library";
+import ProductCard from "./Experiments/exp3_1/ProductCard";
 
 function App() {
-  return <Library />;
+  return (
+    <div style={{ display: "flex", gap: "20px", padding: "40px" }}>
+      <ProductCard
+        name="Wireless Headphones"
+        price="1999"
+        image="https://m.media-amazon.com/images/I/61CGHv6kmWL._SX679_.jpg"
+        inStock={true}
+      />
+
+      <ProductCard
+        name="Smart Watch"
+        price="2999"
+        image="https://m.media-amazon.com/images/I/71Swqqe7XAL._SX679_.jpg"
+        inStock={false}
+      />
+    </div>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/exp3_2/',   // ⚠️ VERY IMPORTANT
+  base: '/exp3_1/',   // ⚠️ VERY IMPORTANT
 })
